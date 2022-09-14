a29ff4a30dd7b4a529b612a39dbafee5a8f62f89
hisagy69 <sinegamy11@gmail.com>
2022-09-14 21:32:25 +0300

    feat: add update point method

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -14,6 +14,21 @@ export default class PointsModel extends Observable{
     this._notify(updateType, update);
   };
 
+  updatePoint = (updateType, update) => {
+    const index = this.#points.findIndex((point) => point.id === update.id);
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting point');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, index),
+      this.#points[index],
+      ...this.#points.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  };
+
   get points() {
     return this.#points;
   }
